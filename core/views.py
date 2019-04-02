dba32ce2d4076ded0980e6406526b0d4c36d3fbc
David Kirk <david.is.kirk@gmail.com>
2019-04-02 16:10:30 -0400

    I had to disable the getabsoluteURL for the question model, it pointed towards a question-detail template we do not have and it broke the site

--- core/views.py
+++ core/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Question, Answer
+from .forms import QuestionForm
+from django.contrib import messages
+ 
+
 
 def index(request):
 
     questions = Question.objects.all()
     answers = Answer.objects.all()
+    
+    if request.method == "POST" and request.user.is_authenticated:
+        form = QuestionForm(request.POST, request.FILES)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+            messages.success(
+                request,
+                f"Thank you for your input!"
+            )
+            return redirect(to='index')
+
+    form = QuestionForm()
 
     return render(request, 'core/index.html', context= {'answers':answers, 'questions':questions})
 
