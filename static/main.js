a66a76ffd0529561722101800df7540bc4aaa775
Craig Brunengraber <brunengraber@hotmail.com>
2019-04-05 13:58:47 -0400

    started to fix js

--- static/main.js
+++ static/main.js
@@ -10,26 +10,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   })
 })
 
-// AJAX for posting
+// AJAX for posting questions
 function questionDetail () {
   console.log('create question detail page is working!') // sanity check
 }
-    // .ajax({
-    //   url: 'question_detail/', // the endpoint
-    //   type: 'POST', // http method
-    //   data: { the_question : ('#question-text').val() }, // data sent with the post request
-      // handle a successful response
-      // success: function(json) {
-      //   ('#question-text').val(''); // remove the value from the input
-      //   console.log(json); // log the returned json to the console
-        // ('#talk').prepend('<li><strong>'+json.text+'</strong> - <em> '+json.author+'<em> - <span> '+json.created+'</span> - <a id='delete-post-'+json.postslug+''>delete me</a></li>');
-        // console.log("success");
-      // },
-      // handle a non-successful response
-//       error : function(xhr,errmsg,err) {
-//           ('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
-//               " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
-//           console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-//       }
-//   });
-// };
