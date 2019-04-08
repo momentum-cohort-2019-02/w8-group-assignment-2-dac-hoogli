e5026290fa12040dd99118abca2ca1a98f6b5ce1
Jay Spain <meanderingjasmine@gmail.com>
2019-04-08 17:37:22 -0400

    some progress

--- static/main.js
+++ static/main.js
@@ -23,18 +23,6 @@ function questionFunction () {
 
 
 
-var popup1 = document.getElementById("popup-1")
-var openPopup1 = document.getElementById("open-popup-1")
-var closePopup1 = document.getElementById('close-popup-1')
-
-openPopup1.addEventListener('click', () => {
-	popup1.style.display = "block";
-})
-
-closePopup1.addEventListener('click', () => {
-	popup1.style.display = "none";
-})
-
 const Cookies = require('js-cookie')
 const deepmerge = require('deepmerge')
 
